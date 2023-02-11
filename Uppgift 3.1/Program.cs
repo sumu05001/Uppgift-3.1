45079492dcc8d0697a0b8593e706408813d8d298
Tsnem Musa <tm222qf@student.lnu.se>
2023-02-11 18:08:09 +0100

    Add project files.

---
+++ Uppgift 3.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace uppgift1
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hur gammal du är ?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder < 16)
+            {
+                Console.WriteLine("Du är för ung");
+            }
+            else if (ålder > 19)
+            {
+                Console.WriteLine("Du är för gammal");
+            }
+            else
+            {
+                Console.WriteLine("Du får delta i tävling");
+            }
+        }
+    }
+}
